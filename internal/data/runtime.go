caa936095bbbb5cd380b06308ea50155de071e65
Thinh <15520839@gm.uit.edu.vn>
2026-06-26 21:56:12 +0700

    Validate movies input

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,37 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 func (rt Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", rt)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (rt *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	min, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*rt = Runtime(min)
+	return nil
 }
